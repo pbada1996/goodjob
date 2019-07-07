3ceef936659e24f75200c12bf7734cb907cdc46f
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2019-07-07 02:27:02 -0500

    detalles de boton postular diseño

--- goodjob/app/src/main/java/com/example/goodjob/ProfileFragment.java
+++ goodjob/app/src/main/java/com/example/goodjob/ProfileFragment.java
@@ -1,14 +1,10 @@
 package com.example.goodjob;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
 
 public class ProfileFragment extends Fragment {
 
@@ -22,6 +18,10 @@ public class ProfileFragment extends Fragment {
 
         // TODO: programar para checkar el perfil de uno mismo y de los usuarios aceptados
 
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            Integer idUsuario = bundle.getInt("id");
+        }
         return view;
     }
 
