52de394a561769979eeb567b3e83945f0535adbd
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2019-11-04 18:40:10 -0500

    agregados spinners. Esperando a modificar la db para tocar el proceso de publicar y el service

--- goodjob/app/src/main/java/com/example/goodjob/PublicarActividadActivity.java
+++ goodjob/app/src/main/java/com/example/goodjob/PublicarActividadActivity.java
@@ -1,14 +1,24 @@
 package com.example.goodjob;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,6 +29,8 @@ import com.android.volley.toolbox.Volley;
 import com.example.goodjob.classes.ValidSession;
 import com.example.goodjob.util.DatePickerFragment;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.regex.Pattern;
 
 public class PublicarActividadActivity extends AppCompatActivity {
@@ -30,12 +42,15 @@ public class PublicarActividadActivity extends AppCompatActivity {
     private EditText recompensa;
     private Button publicarActividad;
     private TextInputLayout tilNombreActividad, tilDescripcionActividad,
-            tilFecha, tilParticipantesRequeridos;
+            tilFecha, tilParticipantesRequeridos, tilRecompensa;
+    private ImageView imagenActividad;
+    private Spinner tipoSeleccion, tipoRecompensa;
 
     private final int CAMPO_VACIO = 0;
     final int NOMBRE_ACTIVIDAD_TOPE_LONGITUD = 30;
     final int DESCRIPCION_ACTIVIDAD_TOPE_LONGITUD = 140;
     final int PARTICIPANTES_MINIMOS = 2;
+    final int REQUEST_CODE_GALLERY = 78;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +58,11 @@ public class PublicarActividadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_publicar_actividad);
 
         mapearCampos();
+        establecerAdaptadorSpinnerSeleccion();
+        establecerAdaptadorSpinnerRecompensa();
         establecerEventosDeChekeoDeCampos();
         asignarEventoDeClickParaLaFecha();
+        asignarEventoDeClickParaImagen();
         asignarEventoDeClickEnBotonPublicarActividad();
     }
 
@@ -59,6 +77,26 @@ public class PublicarActividadActivity extends AppCompatActivity {
         tilDescripcionActividad = findViewById(R.id.tilDescripcionActividad);
         tilFecha = findViewById(R.id.tilFecha);
         tilParticipantesRequeridos = findViewById(R.id.tilCantidadIntegrantes);
+        imagenActividad = findViewById(R.id.ivImagenActividad);
+        tilRecompensa = findViewById(R.id.tilRecompensa);
+        tipoSeleccion = findViewById(R.id.spinnerTipoSeleccion);
+        tipoRecompensa = findViewById(R.id.spinnerTipoRecompensa);
+    }
+
+    private void establecerAdaptadorSpinnerSeleccion(){
+        ArrayAdapter<CharSequence> adapterSeleccion =
+                ArrayAdapter.createFromResource(this,
+                        R.array.tipo_seleccion_array,
+                        R.layout.item_spinner_publicar_actividad);
+        tipoSeleccion.setAdapter(adapterSeleccion);
+    }
+
+    private void establecerAdaptadorSpinnerRecompensa(){
+        ArrayAdapter<CharSequence> adapterRecompensa =
+                ArrayAdapter.createFromResource(this,
+                        R.array.tipo_recompensa_array,
+                        R.layout.item_spinner_publicar_actividad);
+        tipoRecompensa.setAdapter(adapterRecompensa);
     }
 
     private void asignarEventoDeClickEnBotonPublicarActividad() {
@@ -100,7 +138,8 @@ public class PublicarActividadActivity extends AppCompatActivity {
         return esNombreActividadValido(nombreActividad.getText().toString())
                 && esDescripcionValida(descripcionActividad.getText().toString())
                 && esFechaValida(fechaFin.getText().toString())
-                && esCantidadParticipantesValida(cantidadParticipantes.getText().toString());
+                && esCantidadParticipantesValida(cantidadParticipantes.getText().toString())
+                && esRecompensaValida(recompensa.getText().toString());
     }
 
     private void establecerEventosDeChekeoDeCampos() {
@@ -152,9 +191,25 @@ public class PublicarActividadActivity extends AppCompatActivity {
 
             }
         });
+        recompensa.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                esRecompensaValida(String.valueOf(charSequence));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
-    private void asignarEventoDeClickParaLaFecha(){
+    private void asignarEventoDeClickParaLaFecha() {
         fechaFin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -187,8 +242,8 @@ public class PublicarActividadActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean esFechaValida(String fecha){
-        if (fecha.length() == CAMPO_VACIO){
+    private boolean esFechaValida(String fecha) {
+        if (fecha.length() == CAMPO_VACIO) {
             tilFecha.setError("Ingrese una fecha");
             return false;
         }
@@ -196,18 +251,81 @@ public class PublicarActividadActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean esCantidadParticipantesValida(String cantidadParticipantes){
-        if (cantidadParticipantes.trim().length() == CAMPO_VACIO){
+    private boolean esCantidadParticipantesValida(String cantidadParticipantes) {
+        if (cantidadParticipantes.trim().length() == CAMPO_VACIO) {
             tilParticipantesRequeridos.setError("Ingrese cantidad de participantes");
             return false;
-        } else if (Integer.valueOf(cantidadParticipantes) < PARTICIPANTES_MINIMOS){
-            tilParticipantesRequeridos.setError("El mínimo de participantes es 2");
-            return false;
-        } else if (!cantidadParticipantes.trim().matches("^[0-9]+$")){
+        } else if (!cantidadParticipantes.trim().matches("^[0-9]+$")) {
             tilParticipantesRequeridos.setError("Solo se admiten números");
+            return false;
+        } else if (Integer.valueOf(cantidadParticipantes) < PARTICIPANTES_MINIMOS) {
+            tilParticipantesRequeridos.setError("El mínimo de participantes es 2");
             return false;
         }
         tilParticipantesRequeridos.setError(null);
         return true;
+    }
+
+    private boolean esRecompensaValida(String recompensa){
+        if (recompensa.trim().length() == CAMPO_VACIO){
+            tilRecompensa.setError("Ingrese recompensa");
+            return false;
+        } else if (!recompensa.trim().matches("^[0-9]+$")){
+            tilRecompensa.setError("Ingrese una recompensa valida");
+            return false;
+        }
+        tilRecompensa.setError(null);
+        return true;
+    }
+
+    private void asignarEventoDeClickParaImagen() {
+        imagenActividad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                solicitarPermisoLectura();
+            }
+        });
+    }
+
+    private void solicitarPermisoLectura() {
+        ActivityCompat.requestPermissions(PublicarActividadActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_CODE_GALLERY);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                abrirGaleria();
+            } else {
+                Toast.makeText(this, "Permisos no concedidos", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            try {
+                setearImagenSeleccionada(data);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setearImagenSeleccionada(Intent data) throws FileNotFoundException{
+        InputStream input = getContentResolver().openInputStream(data.getData());
+        Bitmap bitmap = BitmapFactory.decodeStream(input);
+        imagenActividad.setImageBitmap(bitmap);
     }
 }
