8159c5048d66d2145387c223a437a9f0106cb104
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2019-11-16 20:58:38 -0500

    diseno solicitud de empresa

---
+++ goodjob/app/src/main/java/com/example/goodjob/SolicitudEmpresaActivity.java
@@ -0,0 +1,13 @@
+package com.example.goodjob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitudEmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_empresa);
+    }
+}
