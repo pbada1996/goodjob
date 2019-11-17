a66473f8f8d8b8c5590e954a24394eb55e45e6d9
bratzon <you@example.com>
2019-11-16 21:23:07 -0500

    diseño listado por corregir

--- goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
+++ goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
@@ -3,5 +3,5 @@ package com.example.goodjob.classes;
 public class ValidSession {
 
     public static User usuarioLogueado = null;
-    public static final String IP = "https://192.168.1.5/Conexiones";
+    public static final String IP = "https://192.168.42.159/Conexiones";
 }
