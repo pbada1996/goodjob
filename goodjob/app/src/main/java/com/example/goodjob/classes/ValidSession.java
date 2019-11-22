45edb4bc5f47136cd6d770988feab6d1750f5b0f
gustavo <lizarzaburumercado@gmail.com>
2019-11-21 22:32:29 -0500

    postular-actividad

--- goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
+++ goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
@@ -3,7 +3,7 @@ package com.example.goodjob.classes;
 public class ValidSession {
 
     public static User usuarioLogueado = null;
-    private static final String LOCAL = "https://192.168.1.5";
+    private static final String LOCAL = "https://10.205.41.101";
     public static final String IP = LOCAL + "/Conexiones";
     public static final String IP_IMAGENES_ACTIVIDADES = LOCAL + "/imagenes-actividades";
     public static final String IMAGENES_ACTIVIDADES_GUARDADAS = IP_IMAGENES_ACTIVIDADES + "/imagenes-subidas";
