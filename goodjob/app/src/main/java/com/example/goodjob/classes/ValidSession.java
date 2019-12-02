275e0c4827828f4fc7fd02da731091429218c6eb
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2019-12-02 11:07:01 -0500

    refactorizado nombres de carpetas donde se alojan las imagenes. servicios y variables acordes

--- goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
+++ goodjob/app/src/main/java/com/example/goodjob/classes/ValidSession.java
@@ -8,7 +8,7 @@ public class ValidSession {
     // la línea debajo usa la conexión remota
     // private static final String LOCAL = "https://anthostudio.ga";
     public static final String IP = LOCAL + "/Conexiones";
-    public static final String IP_IMAGENES = LOCAL + "/imagenes-actividades";
-    public static final String IMAGENES_ACTIVIDADES = IP_IMAGENES + "/imagenes-subidas/";
+    public static final String IP_IMAGENES = LOCAL + "/imagenes";
+    public static final String IMAGENES_ACTIVIDADES = IP_IMAGENES + "/imagenes_actividades/";
     public static final String IMAGENES_PRODUCTOS = IP_IMAGENES + "/imagenes_productos/";
 }
